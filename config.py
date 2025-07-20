004a49dbc04d3ff79ab5a6ad562bccb9df0c18d2
Sarvesh Patil <127282376+shutuplexus@users.noreply.github.com>
2025-07-20 14:28:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     "Config class for variables"
-    SECRET_KEY = "sarvesh" # add your own secret key eg. userxd etc
-    MONGO_URI = "mongodb+srv://redhoenix:adifeb72@adidb.z5wtg.mongodb.net/?retryWrites=true&w=majority&appName=AdiDB" #add your own mongo db uri from mongodb.com
+    SECRET_KEY = "" # add your own secret key eg. userxd etc
+    MONGO_URI = "" #Add your own mongo db uri from mongodb.com
