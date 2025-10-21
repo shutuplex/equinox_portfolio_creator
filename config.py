18422e5254b79e2b09f2918088b45ebace8fa8da
Sarvesh Patil <127282376+shutuplex@users.noreply.github.com>
2025-10-21 23:39:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 class Config:
     "Config class for variables"
     SECRET_KEY = "sddsds" # add your own secret key eg. userxd etc
-    MONGO_URI = "mongodb+srv://ffharuhiko:haruhiko6934@lexusdb.5ucdk.mongodb.net/?retryWrites=true&w=majority&appName=LexusDB" #Add your own mongo db uri from mongodb.com
+    MONGO_URI = "" #Add your own mongo db uri from mongodb.com
+
 
